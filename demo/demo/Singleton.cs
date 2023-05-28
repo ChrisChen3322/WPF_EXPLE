1cc533c5e81aa1542b70545cca1187a57769979a
chris chen <134181537+ChrisChen3322@users.noreply.github.com>
2023-05-28 17:18:26 +0800

    Add files via upload

---
+++ demo/demo/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace demo
+{
+    public sealed class SingletonPattern
+    {
+        private static readonly SingletonPattern _sigleton = new SingletonPattern();
+
+        //获取唯一可用的对象
+        public static SingletonPattern getInstance()
+        {
+            return _sigleton;
+        }
+
+        public void showMessage(string p_txt = "")
+        {
+            MessageBox.Show(p_txt);
+        }
+    }
+}
